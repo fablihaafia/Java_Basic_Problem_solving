02d368c96f664eae2751dd5f23e7f0648dd895a5
fablihaafia <fabliha1afia@gmail.com>
2025-04-16 11:58:18 +0600

    LinkedSetHashExample

---
+++ LinkedHashSetExample.java
@@ -0,0 +1,22 @@
+package Java_Basic_Problem_Solving;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetExample {
+
+	public static void main(String[] args) {
+		LinkedHashSet <Integer> ages = new LinkedHashSet<Integer>();
+		
+        ages.add(20);
+        ages.add(18);
+        ages.add(28);
+        ages.add(50);
+        
+        for(Integer age : ages) {
+        	System.out.println("People age is :" + age);
+        	
+        }
+
+	}
+
+}
