d76b1bc6f76499860a63897935729712c20a88bf
fablihaafia <fabliha1afia@gmail.com>
2025-04-16 12:02:17 +0600

    Loop

---
+++ LoopExample.java
@@ -0,0 +1,14 @@
+package Java_Basic_Problem_Solving;
+
+public class LoopExample {
+
+	public static void main(String[] args) {
+		 System.out.println("Counting from 1 to 5:");
+
+         for (int i = 1; i <= 5; i++) {
+             System.out.println("Number: " + i);
+         } 
+
+	}
+
+}
