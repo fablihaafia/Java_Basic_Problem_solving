4267d902e6298792d6e97ef6e3b82222c68201a1
fablihaafia <fabliha1afia@gmail.com>
2025-04-16 11:54:03 +0600

    OddEven

---
+++ EvenOddNumber.java
@@ -0,0 +1,27 @@
+package Java_Basic_Problem_Solving;
+
+import java.util.Scanner;
+
+public class EvenOddNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number;
+		
+		System.out.println("Enter the number");
+		
+		number  = sc.nextInt();
+		if(number%2 == 0) {
+			System.out.println("Even number");
+		}
+		else {
+			System.out.println("Odd number");
+		}
+		
+		 sc.close();
+		
+		
+
+	}
+
+}
